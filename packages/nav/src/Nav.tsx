14086b1fe5d44a991dfe80092006021e3734d18e
Jhonnatan <jjhoncv@gmail.com>
2024-12-09 14:27:25 -0500

    Actualizar Nav.tsx

--- packages/nav/src/Nav.tsx
+++ packages/nav/src/Nav.tsx
@@ -132,7 +132,7 @@ export function Nav({
               )}
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
+              strokeWidth={1}
               d="M6 18L18 6M6 6l12 12"
             />
           ) : (
@@ -143,7 +143,7 @@ export function Nav({
               )}
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
+              strokeWidth={1}
               d="M4 6h16M4 12h16M4 18h16"
             />
           )}
