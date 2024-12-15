4f502c861c1b5e3cf07c272e898a5c1cba6119d0
Jhonnatan <jjhoncv@gmail.com>
2024-12-15 11:53:18 -0500

    Actualizar Sheet.tsx

--- packages/nav/src/Sheet.tsx
+++ packages/nav/src/Sheet.tsx
@@ -1,21 +1,35 @@
 import { type FC } from "react";
 
 interface SheetProps {
-  isOpen: boolean;
+  isOpen?: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  side?: "left" | "right"
 }
 
-export const Sheet:FC<SheetProps> = ({ isOpen, onClose, children }) => {
+export const Sheet:FC<SheetProps> = ({ 
+  isOpen = false, 
+  onClose,
+  children,
+  side = "left"
+  }) => {
   
+  const sideStyles = {
+    left: "left-0 translate-x-[-100%]",
+    right: "right-0 translate-x-[100%]"
+  };
+
+  const baseStylesMobile = {
+    box: "md:invisible duration-300 transition-all shadow-2xl w-[80%] fixed z-40 h-dvh top-0 px-4 py-2",
+    bg: "bg-white"
+  };
 
   return (
       <div
         className={cn(
           baseStylesMobile.box,
           baseStylesMobile.bg,
-          variantBoxMobileStyles[variantBoxMobile][variant].box,
-          variantBoxMobileStyles[variantBoxMobile][variant].bg,
+          sideStyles[side],
           isOpen ? "translate-x-[0%] transition-all" : ""
         )}
       >
