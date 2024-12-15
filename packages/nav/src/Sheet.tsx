6da55fe2668d5e874c9463508c0f6dc397758021
Jhonnatan <jjhoncv@gmail.com>
2024-12-15 11:32:37 -0500

    Actualizar Sheet.tsx

--- packages/nav/src/Sheet.tsx
+++ packages/nav/src/Sheet.tsx
@@ -3,9 +3,23 @@ import { type FC } from "react";
 interface SheetProps {
   isOpen: boolean;
   onClose: () => void;
-   
+  children: React.ReactNode;
 }
 
-export const Sheet:FC<SheetProps> = ({ isOpen, onClose }) => {
-  return <div>sheet</div>
+export const Sheet:FC<SheetProps> = ({ isOpen, onClose, children }) => {
+  
+
+  return (
+      <div
+        className={cn(
+          baseStylesMobile.box,
+          baseStylesMobile.bg,
+          variantBoxMobileStyles[variantBoxMobile][variant].box,
+          variantBoxMobileStyles[variantBoxMobile][variant].bg,
+          isOpen ? "translate-x-[0%] transition-all" : ""
+        )}
+      >
+       {children}
+      </div>
+  )
 }
